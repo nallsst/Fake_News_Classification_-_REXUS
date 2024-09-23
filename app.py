b8ad3b120fcb1899980b8fa57c0b19b8c6440e5d
nallsst <115079115+nallsst@users.noreply.github.com>
2024-09-24 05:25:27 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+from ml_app import run_ml_app
+
+def main():
+    menu = ['Home', 'Machine Learning']
+    choice = st.sidebar.selectbox('Menu', menu)
+
+    if choice == 'Home':
+        st.subheader('Welcome to Rexus Final Project')
+        st.write("Click the selecbox on the left")
+        st.write("And then choose 'Machine Learning' to start Classification  on Fake News")
+        st.write("Here is the link of Fake News Dataset: https://www.kaggle.com/datasets/rajatkumar30/fake-news")
+    elif choice == 'Machine Learning':
+        #st.subheader('Welcome to Our Machine Learning')
+        run_ml_app()
+
+        
+if __name__ == '__main__':
+    main()
---
+++ ml_app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+import pickle
+import os
+from nltk.stem import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+def load_file_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
+
+def run_ml_app():
+  st.subheader('Fake News Classification')
+  st.write("This model uses Lemmatization as Data Preprocessing, TF-IDF as Feature Engineering and XGBoost for Accuracy.")
+  input_title = st.text_area("Insert news title")
+  input_content = st.text_area("Insert news content")
+  if st.button("Classify"):
+    # combine
+    combined_title_content = input_title + " " + input_content
+    # tokenize
+    tokenized_news = word_tokenize(combined_title_content)
+    # stop words
+    stop_words = set(stopwords.words('english'))
+    filtered_news = [word for word in tokenized_news if word.lower() not in stop_words]
+    # lemmatize
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_news = [lemmatizer.lemmatize(token) for token in filtered_news]
+    # joining into text
+    lemmatized_text = ' '.join(lemmatized_news)
+    # vectorizer
+    vectorizer = load_file_pickle('vectorizer.pkl')
+    ready_news = vectorizer.transform([lemmatized_text])
+    # Prediction
+    st.subheader('Prediction Result')
+    model = load_file_pickle('model_xgboost.pkl')
+
+    prediction = model.predict(ready_news)
+    pred_proba = model.predict_proba(ready_news)
+    
+    pred_probability_score = {'Real News percentage':round(pred_proba[0][1]*100,4),
+                                'Fake News percentage':round(pred_proba[0][0]*100,4)}
+
+    if prediction == 1:
+      st.success("It's real news")
+      st.write(pred_probability_score)
+    else:
+      st.warning("It's fake news")
+      st.write(pred_probability_score)
